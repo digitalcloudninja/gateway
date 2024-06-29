e31331df1085db7214cb36e87c1b4ca52b55100b
DigitalCloud Ninja <admin@digitalcloud.ninja>
2024-06-28 23:48:34 -0600

    Initial Commit

--- src/main/java/ninja/digitalcloud/cloud/gateway/Main.java
+++ src/main/java/ninja/digitalcloud/cloud/gateway/Main.java
@@ -1,4 +1,4 @@
-package ninja.digitalcloud.cloud.starter;
+package ninja.digitalcloud.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
